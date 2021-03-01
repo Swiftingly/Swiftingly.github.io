32836d6b12f5d6657a209920066eef86431ba624
Swiftingly <34215520+Swiftingly@users.noreply.github.com>
2021-02-28 23:25:48 -0500

    Pretty

--- wordsearch.js
+++ wordsearch.js
@@ -15,7 +15,7 @@ function draw(positions) {
 	let xSpace = 600 / width;
 	let ySpace = 600 / height;
 	var size = Math.floor(Math.min(xSpace, ySpace));
-	ctx.font = String(size) + "px Courier New";
+	ctx.font = String(size) + "px Consolas";
 	
 	ctx.beginPath();
 	for (let i = 0; i <= width; i++) {
@@ -28,27 +28,41 @@ function draw(positions) {
 	}
 	ctx.stroke();
 	
-	
+	//var grad = ctx.createLinearGradient(0, 0, 600, 0);
+	//grad.addColorStop(0, "red");
+	//grad.addColorStop(1, "yellow");
+	//ctx.strokeStyle = grad;
 	ctx.strokeStyle = "red";
 	ctx.globalAlpha = 0.5;
+	
 	for (let i = 0; i < positions.length; i++) {
 		ctx.beginPath();
-		ctx.strokeStyle = `hsla(${Math.random() * 360}, 100%, 50%, 0.5)`;
 		
-		var w = positions[i];
-		var xStart = (w.x1 + 0.5) * xSpace;
-		var yStart = (w.y1 + 0.5) * ySpace;
-		var xEnd = (w.x2 + 0.5) * xSpace;
-		var yEnd = (w.y2 + 0.5) * ySpace;
-		var xMid = (xStart + xEnd) / 2;
-		var yMid = (yStart + yEnd) / 2;
-		var dx = xEnd - xStart;
-		var dy = yEnd - yStart;
-		var dist = Math.sqrt(dx * dx + dy * dy);
-		ctx.ellipse(xMid, yMid, (dist / 2 + xSpace / 2) - 10, (size / 2) - 10, Math.atan2(dy, dx), 0, Math.PI * 2);
-		ctx.stroke();
+		var p = positions[i];
+		var x = (p[0]) * xSpace + 10;
+		var y = (p[1]) * ySpace + 10;
+		var dx = p[2] - p[0];
+		var dy = p[3] - p[1];
+		var w = Math.sqrt(dx * dx + dy * dy) * xSpace - 20;
+		var h = ySpace - 20;
 		
-		//ctx.setTransform(1, 0, 0, 1, 0, 0);
+		var offX = (p[0] + 0.5) * xSpace;
+		var offY = (p[1] + 0.5) * ySpace;
+		
+		var rot = Math.atan2(dy * ySpace, dx * xSpace);
+		
+		var grad = ctx.createLinearGradient(x, y, x + w, y);
+		grad.addColorStop(0, "red");
+		grad.addColorStop(1, "blue");
+		ctx.strokeStyle = grad;
+		
+		ctx.translate(offX, offY);
+		ctx.rotate(rot);
+		ctx.translate(-offX, -offY);
+		
+		ctx.strokeRect(x, y, w, h);
+		
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
 	}
 	ctx.globalAlpha = 1;
 	
@@ -85,7 +99,7 @@ function solve() {
 				var dir = dirs[d];
 				var full = findWord(x + dir[0], y + dir[1], word, dir, 1, row[x]);
 				if (full !== "") {
-					positions.push({ x1: x, y1: y, x2: x + dir[0] * (word.length - 1), y2: y + dir[1] * (word.length - 1)});
+					positions.push([ x, y, x + dir[0] * (word.length), y + dir[1] * (word.length) ]);
 					words.push(`${full} x${x} y${y}`);
 				}
 			}
