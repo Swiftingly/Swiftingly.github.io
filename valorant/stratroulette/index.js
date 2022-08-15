559aecd68a2ea33d8b77692ded1761def7e34c01
Swiftingly <34215520+Swiftingly@users.noreply.github.com>
2022-08-15 17:17:52 -0400

    fucky wucky

--- valorant/stratroulette/index.js
+++ valorant/stratroulette/index.js
@@ -2,20 +2,52 @@
 
 (() => {
 
-const strats = [
-	'Kill yourself',
-	'End it',
-	'Stop living'
+function shuffle(array) {
+	let currentIndex = array.length,  randomIndex;
+	// While there remain elements to shuffle.
+	while (currentIndex != 0) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+		array[randomIndex], array[currentIndex]];
+	}
+	
+	return array;
+}
+
+const allStrats = [
+	'SUICIDE:Kill yourself',
+	'FORFEIT:End it',
+	'GULP:Stop breathing',
+	'CULT MOMENT:All alive players kill themselves',
+	'PEER PRESSURE:All dead players pressure the alive players to kill themselves'
 ];
 
+var strats = shuffle([...allStrats]);
+//const currentStrats
+
+const outputTitle = document.getElementById('output-title');
 const output = document.getElementById('output');
 
-output.addEventListener('click', () => {
-	output.style.animation = 'none';
-	output.offsetHeight;
-	output.style.animation = null;
+function reanimate(e) {
+	e.style.animation = 'none';
+	e.offsetHeight;
+	e.style.animation = null;
+}
+
+function roll() {
+	reanimate(output);
 	
-	output.innerText = strats[Math.floor(Math.random() * strats.length)];
-});
+	if (strats.length <= 0) strats = shuffle([...allStrats]);
+	const strat = strats.pop().split(':');
+	outputTitle.innerText = strat[0];
+	output.innerText = strat[1];
+}
+
+outputTitle.addEventListener('click', roll);
+output.addEventListener('click', roll);
 
 })();
