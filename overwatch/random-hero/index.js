23c7fe5cafdceb402f50586cebbfb739d8f3adab
Swiftingly <swiftinglyswift@gmail.com>
2026-09-04 22:39:04 -0400

    added d.mon to ow

--- overwatch/random-hero/index.js
+++ overwatch/random-hero/index.js
@@ -1,4 +1,4 @@
-const TANK_LIST = [ "Mauga", "Orisa", "Roadhog", "Zarya", "D.Va", "Doomfist", "Hazard", "Winston", "Wrecking Ball", "Domina", "Junker Queen", "Ramattra", "Reinhardt", "Sigma" ].sort();
+const TANK_LIST = [ "Mauga", "Orisa", "Roadhog", "Zarya", "D.Va", "Doomfist", "Hazard", "Winston", "Wrecking Ball", "Domina", "Junker Queen", "Ramattra", "Reinhardt", "Sigma", "D.Mon" ].sort();
 const DAMAGE_LIST = [ "Anran", "Genji", "Reaper", "Tracer", "Vendetta", "Venture", "Echo", "Freja", "Pharah", "Sombra", "Ashe", "Cassidy", "Hanzo", "Sojourn", "Widowmaker", "Bastion", "Emre", "Junkrat", "Mei", "Soldier: 76", "Symmetra", "Torbjörn", "Sierra", "Shion" ].sort();
 const SUPPORT_LIST = [ "Kiriko", "Lifeweaver", "Mercy", "Moira", "Brigitte", "Illari", "Juno", "Mizuki", "Wuyang", "Ana", "Baptiste", "Jetpack Cat", "Lúcio", "Zenyatta" ].sort();
 const FULL_LIST = [...TANK_LIST, ...DAMAGE_LIST, ...SUPPORT_LIST].sort();
